7fefe398d2fd91b0b7f4d081c8e33d5034349eac
Chirayu Desai <chirayudesai1@gmail.com>
2020-04-25 04:07:25 +0530

    flash.android.com: prng4.js
    http://www-cs-students.stanford.edu/~tjw/jsbn/

--- nginx/flash.android.com/app.js
+++ nginx/flash.android.com/app.js
@@ -1,37 +1,3 @@
-function Arcfour() {
-    this.i = 0;
-    this.j = 0;
-    this.S = new Array
-}
-function ARC4init(key) {
-    var i, j, t;
-    for (i = 0; i < 256; ++i)
-        this.S[i] = i;
-    j = 0;
-    for (i = 0; i < 256; ++i) {
-        j = j + this.S[i] + key[i % key.length] & 255;
-        t = this.S[i];
-        this.S[i] = this.S[j];
-        this.S[j] = t
-    }
-    this.i = 0;
-    this.j = 0
-}
-function ARC4next() {
-    var t;
-    this.i = this.i + 1 & 255;
-    this.j = this.j + this.S[this.i] & 255;
-    t = this.S[this.i];
-    this.S[this.i] = this.S[this.j];
-    this.S[this.j] = t;
-    return this.S[t + this.S[this.i] & 255]
-}
-Arcfour.prototype.init = ARC4init;
-Arcfour.prototype.next = ARC4next;
-function prng_newstate() {
-    return new Arcfour
-}
-var rng_psize = 256;
 var rng_state;
 var rng_pool;
 var rng_pptr;
